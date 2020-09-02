3e57940c9e4fa7e62862f9729e79d9f925e95b4b
Scott Shepherd <skot@pobox.com>
2020-09-02 08:40:09 -0400

    fixing Millicast

--- src/the-journey/views/Broadcast/Millicast.tsx
+++ src/the-journey/views/Broadcast/Millicast.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Millicast() {
   return (
-      <iframe title="broadcast"
-              src="https://viewer.millicast.com/v2?streamId=Wu3EU2/keidk0k0&muted=true&autoPlay=true"
-              allowFullScreen width="100%" height="100%"
-      />
+    <iframe
+      src="https://viewer.millicast.com/v2?streamId=wbfwt8/ke434gcy&muted=true&autoPlay=true"
+      allowFullScreen width="100%" height="100%"
+    />
   );
 }
