77c30ec40bbdfd4c4dc8c4e2f403f4947be1120a
Scott Shepherd <skot@pobox.com>
2020-09-10 23:01:25 -0400

    click-blocking layer over millicast

--- src/the-journey/views/Broadcast/Millicast.tsx
+++ src/the-journey/views/Broadcast/Millicast.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { styled } from '@material-ui/core';
+
+const ClickBlocker = styled('div')(() => ({
+  position: 'absolute',
+  height: '90vh',
+  width: '100%',
+  background: 'transparent',
+}));
+
+const Container = styled('div')(() => ({
+  position: 'absolute',
+  height: '100%',
+  width: '100%'
+}))
 
 export default function Millicast() {
   return (
+    <>
+      <Container>
     <iframe title="millicast"
       src="https://viewer.millicast.com/v2?streamId=wbfwt8/ke434gcy&muted=true&autoPlay=true"
       allowFullScreen width="100%" height="100%"
     />
+      </Container>
+    <ClickBlocker onClick={(e) => {
+      console.log('click blocker!');
+      e.stopPropagation()
+    }}/>
+    </>
   );
 }
