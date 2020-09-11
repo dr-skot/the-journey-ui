6d75a3a4b17b63104cf46e8ec5660661e66f91ec
Scott Shepherd <skot@pobox.com>
2020-09-10 22:09:33 -0400

    only redirect to https on dev

--- server.js
+++ server.js
@@ -30,28 +30,30 @@ SUBSCRIBE_RULES = {
 }
 
 
+// TODO wrap the above in isDev?
+const isDev = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+if (!isDev()) {
+
 // Enable reverse proxy support in Express. This causes the
 // the "X-Forwarded-Proto" header field to be trusted so its
 // value can be used to determine the protocol. See
 // http://expressjs.com/api#app-settings for more details.
-app.enable('trust proxy');
+  app.enable('trust proxy');
 
 // Add a handler to inspect the req.secure flag (see
 // http://expressjs.com/api#req.secure). This allows us
 // to know whether the request was via http or https.
-app.use (function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
+  app.use(function(req, res, next) {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
 
-// TODO wrap the above in isDev?
-const isDev = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
+}
 
 app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || 8081;
