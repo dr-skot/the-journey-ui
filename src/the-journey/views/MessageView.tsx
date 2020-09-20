e1ab68ff3c9ea7f9e5083746e751d7f6868c89f9
Scott Shepherd <skot@pobox.com>
2020-09-20 15:37:56 -0400

    MessageView element missing from last commit

---
+++ src/the-journey/views/MessageView.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from 'react';
+import { Container, Link, Typography, Paper, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { listKey } from '../utils/react-help';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: '2.5em',
+  },
+  paper: {
+    padding: '1em',
+  },
+  heading: {
+    marginBottom: '0.4em',
+    textAlign: 'center',
+  },
+  paragraph: {
+    '& a': {
+      color: 'inherit',
+    }
+  }
+});
+
+interface SimpleMessageProps {
+  title: string,
+  paragraphs?: (string | ReactNode)[],
+}
+
+export default function SimpleMessage({ title, paragraphs = [] }: SimpleMessageProps) {
+  const classes = useStyles();
+    return (
+      <Container>
+        <Grid container justify="center" className={classes.container}>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+              <Typography variant="h4" className={classes.heading}>
+                { title }
+              </Typography>
+              { paragraphs.map((paragraph, i) => (
+                <Typography key={listKey('paragraph', i)} className={classes.paragraph}>
+                  { paragraph }
+                </Typography>
+              )) }
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+}
