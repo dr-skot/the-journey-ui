745f5310a8a5db550ded216758abd56a0077db29
Scott Shepherd <skot@pobox.com>
2020-09-22 10:30:39 -0400

    gear button on comm

--- src/the-journey/views/Comm/Comm.tsx
+++ src/the-journey/views/Comm/Comm.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { getIdentities, isRole } from '../../utils/twilio';
 import useParticipants from '../../hooks/useParticipants/useParticipants';
 import Subscribe from '../../subscribers/Subscribe';
-import CenteredInWindow from '../../components/CenteredInWindow';
-import ToggleAudioButton from '../../components/Controls/ToggleAudioButton/ToggleAudioButton';
 import AutoJoin from '../../components/AutoJoin';
 import WithFacts from '../Facts/WithFacts';
 import PlayAllSubscribedAudio from '../../components/audio/PlayAllSubscribedAudio';
 import Chat from '../../components/Chat/Chat';
+import CenteredInWindow from '../../components/CenteredInWindow';
+import ToggleAudioButton from '../../components/Controls/ToggleAudioButton/ToggleAudioButton';
+import SettingsButton from '../../components/Controls/SettingsButton/SettingsButton';
 
 export default function Comm() {
   const commUsers = getIdentities(useParticipants().filter(isRole('comm')));
-  return (
+  return <>
+    <AutoJoin role="comm" withAudio/>
+    <Subscribe profile="listen" focus={commUsers}/>
+    <PlayAllSubscribedAudio/>
     <WithFacts>
-      <AutoJoin role="comm" withAudio/>
-      <Subscribe profile="listen" focus={commUsers}/>
-      <PlayAllSubscribedAudio/>
-      <CenteredInWindow><ToggleAudioButton/></CenteredInWindow>
+      <CenteredInWindow><ToggleAudioButton/><SettingsButton/></CenteredInWindow>
       <Chat/>
     </WithFacts>
-  )
+  </>
 }
 
