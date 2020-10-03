12a7861a5db0e7e9a7a161042a67a15735f77a61
Scott Shepherd <skot@pobox.com>
2020-10-03 17:04:17 -0400

    will it help just to get the tracks earlier?

--- src/the-journey/views/Entry/Entry.tsx
+++ src/the-journey/views/Entry/Entry.tsx
@@ -5,12 +5,14 @@ import GetMedia from './GetMedia';
 import NameForm from './NameForm';
 import useRoomName from '../../hooks/useRoomName';
 import { Messages } from '../../messaging/messages';
+import { useLocalTracks } from '../../hooks/useLocalTracks';
 
 type MediaStatus = 'pending' | 'ready' | 'help-needed'
 
 export default function Entry({ test }: { test?: boolean }) {
   const [{ room, roomStatus }] = useTwilioRoomContext();
   const roomName = useRoomName() + (test ? '-test' : '');
+  useLocalTracks();
 
   if (!room || roomStatus === 'disconnected') return <NameForm roomName={roomName}/>
 
