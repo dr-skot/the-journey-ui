359b8582c05628ac2ad63cc515ac231afee38449
Scott Shepherd <skot@pobox.com>
2020-10-07 17:07:37 -0400

    strange behavior ui not showing latest version

--- src/the-journey/views/Operator/Operator.tsx
+++ src/the-journey/views/Operator/Operator.tsx
@@ -31,6 +31,8 @@ function OperatorView() {
   const { toggleFocus } = useOperatorControls();
   const { gallery, paged, pageNumber, hideBlanks, menuButtons, order } = usePagedAudience();
 
+  console.log('operator up to date');
+
   const galleryProps = {
     participants: gallery,
     selection: focusGroup,
