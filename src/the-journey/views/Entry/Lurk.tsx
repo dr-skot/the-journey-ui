ec375a2a2f5f5e270e91a45554b07e2f13f1ac67
Scott Shepherd <skot@pobox.com>
2020-10-18 14:33:19 -0400

    lurk experiments

--- src/the-journey/views/Entry/Lurk.tsx
+++ src/the-journey/views/Entry/Lurk.tsx
@@ -3,8 +3,10 @@ import AutoJoin from '../../components/AutoJoin';
 import UnstaffedRoomCheck from '../../components/UnstaffedRoomCheck';
 import WithFacts from '../Facts/WithFacts';
 import Broadcast from '../Broadcast/Broadcast';
+import { useLocalTracks } from '../../hooks/useLocalTracks';
 
 export default function Lurk() {
+  useLocalTracks();
   return <>
     <AutoJoin role="lurker"/>
     <UnstaffedRoomCheck>
